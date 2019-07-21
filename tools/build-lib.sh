ff5069dc3ea3e3fb6726a08fc5e92e27925f7c7f
Andrey Korovin <misticwonder@gmail.com>
2019-07-21 19:11:41 +0300

    complex-filter overlay color

--- tools/build-lib.sh
+++ tools/build-lib.sh
@@ -5,6 +5,11 @@ lib="$1"
 if [[ "$lib" != "" ]] ; then
     rimraf dist/"$lib"
     npx ng build @"$lib"
+
+    scssConfig=libs/"$lib"/scss-bundle.config.json
+    if test -f "$scssConfig"; then
+        npx scss-bundle -c "$scssConfig"
+    fi
 else
     echo "build lib param errors"
     exit 1
