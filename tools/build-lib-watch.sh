dc7c17da703dce6fb95bbac452a67df122dc1cf5
Andrey Korovin <misticwonder@gmail.com>
2019-07-21 23:12:09 +0300

    fix button colors

--- tools/build-lib-watch.sh
+++ tools/build-lib-watch.sh
@@ -4,13 +4,7 @@ lib="$1"
 
 if [[ "$lib" != "" ]] ; then
     rimraf dist/"$lib"
-
-    scssConfig=libs/"$lib"/scss-bundle.config.json
-    if test -f "$scssConfig"; then
-        npx concurrently --kill-others "npx ng build @$lib --watch" "npx scss-bundle -w libs/$lib/src/lib -c $scssConfig"
-    else
-        npx ng build @"$lib" --watch
-    fi
+    npx ng build @"$lib" --watch
 else
     echo "param errors"
     exit 1
