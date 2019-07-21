256bac67c7d64277f21b8daada51fc5d07145644
Andrey Korovin <misticwonder@gmail.com>
2019-07-21 21:48:47 +0300

    concurrently build scss

--- tools/build-lib-watch.sh
+++ tools/build-lib-watch.sh
@@ -4,7 +4,13 @@ lib="$1"
 
 if [[ "$lib" != "" ]] ; then
     rimraf dist/"$lib"
-    npx ng build @"$lib" --watch
+
+    scssConfig=libs/"$lib"/scss-bundle.config.json
+    if test -f "$scssConfig"; then
+        npx concurrently --kill-others "npx ng build @$lib --watch" "npx scss-bundle -w libs/$lib/src/lib -c $scssConfig"
+    else
+        npx ng build @"$lib" --watch
+    fi
 else
     echo "param errors"
     exit 1
