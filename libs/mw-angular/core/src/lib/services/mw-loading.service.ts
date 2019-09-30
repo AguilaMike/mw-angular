8ab873edf4bd924fe9b4255c46af4a087c7e82ca
Andrey Korovin <misticwonder@gmail.com>
2019-09-30 12:07:39 +0300

    development: is loading group

--- libs/mw-angular/core/src/lib/services/mw-loading.service.ts
+++ libs/mw-angular/core/src/lib/services/mw-loading.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subscriber } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable, Subscriber } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Injectable({
@@ -7,18 +7,30 @@ import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 })
 export class MwLoadingService {
   private isLoadingSubjectsPool: { [tag: string]: BehaviorSubject<number> } = {};
+  private readonly generalTag = 'general';
+  private readonly debounceTime = 100;
 
-  getIsLoading(tag = 'general'): Observable<boolean> {
+  getIsLoading(tag = this.generalTag): Observable<boolean> {
     this.checkAndInitIsLoadingSubject(tag);
 
     return this.isLoadingSubjectsPool[tag].asObservable().pipe(
       map((value: number) => value > 0),
       distinctUntilChanged(),
-      debounceTime(100),
+      debounceTime(this.debounceTime),
     );
   }
 
-  startObservable(tag = 'general'): Observable<void> {
+  getIsLoadingGroup(tags = [this.generalTag]): Observable<boolean> {
+    const tagsIsLoading: Observable<boolean>[] = tags.map((tag: string) => this.getIsLoading(tag));
+
+    return combineLatest(tagsIsLoading).pipe(
+      map((loadings: boolean[]) => loadings.some((loader: boolean) => loader)),
+      distinctUntilChanged(),
+      debounceTime(this.debounceTime),
+    );
+  }
+
+  startObservable(tag = this.generalTag): Observable<void> {
     return new Observable((subscriber: Subscriber<void>) => {
       this.start(tag);
 
@@ -29,13 +41,13 @@ export class MwLoadingService {
     });
   }
 
-  start(tag = 'general'): void {
+  start(tag = this.generalTag): void {
     this.checkAndInitIsLoadingSubject(tag);
 
     this.isLoadingSubjectsPool[tag].next(this.isLoadingSubjectsPool[tag].value + 1);
   }
 
-  stop(tag = 'general'): void {
+  stop(tag = this.generalTag): void {
     if (this.isLoadingSubjectsPool[tag] === undefined) {
       throw new Error(`Loading subject was not created for tag: ${tag}.`);
     }
@@ -45,7 +57,7 @@ export class MwLoadingService {
     }
   }
 
-  destroy(tag = 'general'): void {
+  destroy(tag = this.generalTag): void {
     if (this.isLoadingSubjectsPool[tag] !== undefined) {
       this.isLoadingSubjectsPool[tag].next(0);
       this.isLoadingSubjectsPool[tag].complete();
